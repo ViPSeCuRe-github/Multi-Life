e13930aca2c781137b9970b72b26905ca3cc2676
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-12 18:53:16 +0530

    Update recruit-hero.js

--- recruit-hero.js
+++ recruit-hero.js
@@ -1,4 +1,10 @@
 module.exports = ({
   name: "recruit",
-  code: ``
+  code: `$thumbnail[$botAvatae]
+  $title[1; Recruitment Of Heroes ]
+  $description[1;
+  $addField[1;Creo;
+  Price - 100,000 
+  Damage ⚔️ - 50
+  HP ❤️ - 300] `
   })
