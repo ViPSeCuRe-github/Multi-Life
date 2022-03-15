6881c8f3628f09a49a0f4ca29400e8862b5c9a26
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-15 15:52:33 +0530

    Add files via upload

---
+++ gems-1.js
@@ -0,0 +1,20 @@
+module.exports = ({
+  name: "gems",
+  type: "awaitedCommand",
+  code: `$editMessage[$message[1];
+{title: Gems Shop}
+{description:  
+25$customEmoji[gems;837214550255861760] | 5000 Coins$customEmoji[gold;837214550255861760]
+Command: c!buy 5000 gold
+
+45$customEmoji[gems;837214550255861760] | 10000 Coins$customEmoji[gold;837214550255861760]
+Command: c!buy 10000 gold
+
+25$customEmoji[gems;837214550255861760] | 5000 Elixir$customEmoji[elixir;837214550255861760]
+Command: c!buy 5000 elixir
+
+45$customEmoji[gems;837214550255861760] | 10000 Elixir$customEmoji[elixir;837214550255861760]
+Command: c!buy 10000 elixir
+}
+{footer: You Are In Gems Page}]`
+  })
---
+++ shop-1.js
@@ -0,0 +1,14 @@
+module.exports = ({
+  name: "shop",
+  code: `$reactionCollector[$splitText[1];$authorID;1d;🏠,$customEmoji[elixir;837214550255861760],$customEmoji[cannoncart;837214550255861760],$customEmoji[gems;837214550255861760],$customEmoji[INTERSHIP;837214550255861760];shop,resources,defense,gems,in-game;yes]
+$textSplit[$sendMessage[{thumbnail:$userAvatar[$clientID]}
+{title: ClashTube Shop}
+{description: 
+🏠 | Shop
+$customEmoji[elixir;837214550255861760] | Resources
+$customEmoji[cannoncart;837214550255861760] | Defense
+$customEmoji[gems;837214550255861760] | Items Bought By Gems
+$customEmoji[INTERSHIP;837214550255861760] | In-Game Shop
+}
+{footer: You Are In Shop Page};yes]]`
+  })
