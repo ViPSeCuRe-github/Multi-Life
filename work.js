807b957021d400d24c56ef23c13b494939c9b0f6
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-15 13:43:41 +0530

    Update work.js

--- work.js
+++ work.js
@@ -1,5 +1,6 @@
 module.exports = ({
   name: "work",
   code: `You Worked And Earned $random[1000;2000] Multi-Life Coins
-  $setG`
+  $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];$random[1000;2000]]
+  $globalCooldown[20m;You Can Work Again After %time%]`
   })
