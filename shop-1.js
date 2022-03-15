9d123562382c4876aa811261c62072dac59a86c2
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-15 15:55:20 +0530

    Update shop-1.js

--- shop-1.js
+++ shop-1.js
@@ -2,13 +2,13 @@ module.exports = ({
   name: "shop",
   code: `$reactionCollector[$splitText[1];$authorID;1d;🏠,$customEmoji[elixir;837214550255861760],$customEmoji[cannoncart;837214550255861760],$customEmoji[gems;837214550255861760],$customEmoji[INTERSHIP;837214550255861760];shop,resources,defense,gems,in-game;yes]
 $textSplit[$sendMessage[{thumbnail:$userAvatar[$clientID]}
-{title: ClashTube Shop}
+{title: Multi Life Shop}
 {description: 
 🏠 | Shop
-$customEmoji[elixir;837214550255861760] | Resources
-$customEmoji[cannoncart;837214550255861760] | Defense
-$customEmoji[gems;837214550255861760] | Items Bought By Gems
-$customEmoji[INTERSHIP;837214550255861760] | In-Game Shop
+ | Attacking Items
+ | Defense
+ | Gems
+ | People
 }
 {footer: You Are In Shop Page};yes]]`
   })
