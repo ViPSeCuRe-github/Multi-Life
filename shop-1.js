edb06415699d26a4b8211a08840f838417ebeff2
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-15 21:12:41 +0530

    Update shop-1.js

--- shop-1.js
+++ shop-1.js
@@ -1,13 +1,12 @@
 module.exports = ({
   name: "shop",
-  code: `$reactionCollector[$splitText[1];$authorID;1d;$customEmoji[shop;935528503363915826],$customEmoji[missile;935528503363915826],$customEmoji[tank;935528503363915826],$customEmoji[gems;935528503363915826],🫂;shop,attack,defense,gems,people;yes]
+  code: `$reactionCollector[$splitText[1];$authorID;1d;$customEmoji[shop;935528503363915826],$customEmoji[missile;935528503363915826],$customEmoji[tank;935528503363915826],🫂;shop,attack,defense,people;yes]
 $textSplit[$sendMessage[{thumbnail:$userAvatar[$clientID]}
 {title: Multi Life Shop}
 {description: 
 $customEmoji[shop;935528503363915826] | Shop
 $customEmoji[missile;935528503363915826] | Attack
 $customEmoji[tank;935528503363915826] | Defense
-$customEmoji[gems;935528503363915826] | Gems
 🫂| People
 }
 {footer: You Are In Shop Page};yes]]`
