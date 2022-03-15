cd44cd41b770cebb64a3ba5528e506374ffeeefd
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-15 13:00:17 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -12,7 +12,9 @@ World Name: $replaceText[$getGlobalUserVar[World;$findUser[$message]];0;$usernam
 Total Countries: $getGlobalUserVar[Countries;$findUser[$message]]
 Total Presidents: $getGlobalUserVar[Presidents;$findUser[$message]]
 Total Soldiers: $getGlobalUserVar[Soldiers;$findUser[$message]]
-Total People: $getGlobalUserVar[People;$findUser[$message]]]
+Total People: $getGlobalUserVar[People;$findUser[$message]]
+$addField[1;Power;
+Damage ⚔️: $sum[$getGlobalUserVar[Damage];$getGlobalUserVar[HeroDamage]]]
 $color[1;ORANGE]
 $onlyIf[$getGlobalUserVar[Start;$findUser[$message]]==1;$username[$findUser[$message]] Hasn't Started Their Career Yet
 Type m!start To Start Your Career]`})
