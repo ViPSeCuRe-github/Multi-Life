f6aa6824dc210fe81de2be080f8fc448058c1773
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-15 18:28:53 +0530

    Update ccreate.js

--- ccreate.js
+++ ccreate.js
@@ -2,11 +2,11 @@ module.exports = ({
   name: "ccreate",
   code: `You Successfully Created A Clan With Name $message
 $setGlobalUserVar[ClanL;$message/1/0/1/0]
+$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];25000]]
 $onlyIf[$message!=;Invalid Format❌
 c!ccreate ❌
 c!ccreate <name> ✅]
-$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];25000]]
-$onlyIf[$getGlobalUserVar[Coins]>=25000]
+$onlyIf[$getGlobalUserVar[Coins]>=100000;You Do Not Have Enough Coins To Own A Clan]
 $onlyIf[$isNumber[$message]==false;Your Clan Name Should Utleast Contain A Letter]
 $onlyIf[$advancedTextSplit[$getGlobalUserVar[ClanL];/;1]==0;You Are Already In A Clan]
 `
