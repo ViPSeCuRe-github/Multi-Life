009a6b437fa3186c4fdae431bc8ffbdfc60d135b
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 12:18:02 +0530

    Create Await-shop.js

---
+++ Await-shop.js
@@ -0,0 +1,28 @@
+module.exports = ({
+
+  name: "people",
+
+  type: "awaitedCommand",
+
+  code: `$editMessage[$message[1];
+{title: Multi Life Shop}
+
+{description: 
+
+$customEmoji[shop;935528503363915826] | Shop
+
+$customEmoji[missile;935528503363915826] | Attack
+
+$customEmoji[tank;935528503363915826] | Defense
+
+🫂| People
+
+}
+
+{footer: You Are In Shop Page}
+
+
+
+]`
+
+  })
