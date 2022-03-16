87c98876d6cd2045beff55c4e23f6bd44ff89777
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 12:07:41 +0530

    Update Await-people.js

--- Await-people.js
+++ Await-people.js
@@ -4,17 +4,8 @@ module.exports = ({
   code: `$editMessage[$message[1];
 {title: Gems Shop}
 {description:  
-25$customEmoji[gems;837214550255861760] | 5000 Coins$customEmoji[gold;837214550255861760]
-Command: c!buy 5000 gold
-
-45$customEmoji[gems;837214550255861760] | 10000 Coins$customEmoji[gold;837214550255861760]
-Command: c!buy 10000 gold
-
-25$customEmoji[gems;837214550255861760] | 5000 Elixir$customEmoji[elixir;837214550255861760]
-Command: c!buy 5000 elixir
-
-45$customEmoji[gems;837214550255861760] | 10000 Elixir$customEmoji[elixir;837214550255861760]
-Command: c!buy 10000 elixir
+100MLC | Villager
++1 Population • +25MLC In Work Command
 }
-{footer: You Are In Gems Page}]`
+{footer: You Are In People Page}]`
   })
