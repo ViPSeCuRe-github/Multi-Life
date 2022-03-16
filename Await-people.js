b96a6527f3a46bccb0585684e73ddf39278c136a
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 12:26:02 +0530

    Update Await-people.js

--- Await-people.js
+++ Await-people.js
@@ -6,8 +6,12 @@ module.exports = ({
 {description:  
 100MLC | Villager
 +1 Population • +25MLC In Work Command
+ID: villager
 1000MLC | Soldier
 +10 Population • +1 Damage
+ID: soldier
 }
-{footer: You Are In People Page}]`
+{footer: You Are In People Page
+Create Command: $getServerVar[Prefix]create (id) (number of people)
+}]`
   })
