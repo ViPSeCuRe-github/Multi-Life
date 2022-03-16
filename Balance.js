3eddbbd7b55c7ff4976c943b659beecfa9dfd02e
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 12:45:02 +0530

    Update Balance.js

--- Balance.js
+++ Balance.js
@@ -9,6 +9,5 @@ MultiLife Premium Coins: $getGlobalUserVar[PremiumCoins;$findUser[$message]]
 MultiLife Giveaway Coins: $getGlobalUserVar[GiveawayCoins;$findUser[$message]]]
 $color[1;YELLOW]
 $footer[1;Earn,Earn And Earn]
-$onlyIf[$getGlobalUserVar[Start;$findUser[$message]]==1;$username[$findUser[$message]] Hasn't Started Their Career Yet
-Type m!start To Start Your Career]`
+`
 })
