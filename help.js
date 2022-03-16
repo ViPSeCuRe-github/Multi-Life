d174a6cc7f70434cd7443883f23c578e25808eb8
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 13:01:18 +0530

    Create help.js

---
+++ help.js
@@ -0,0 +1,12 @@
+module.exports = ((
+  name: "help",
+  code: `$thumbnail[1;$userAvatar[$botID]]
+  $title[1;Multi-Life Commands]
+  $description[1;
+  $addField[1;Clan;
+  \`ccreate <clan-name>\`,\`cinfo <ownerID>\`,\`join <ownerID>\`,\`leave\`]
+  $addField[1;Earn;
+  \`work\`,\`daily\`]
+  ]`
+  })
+  
