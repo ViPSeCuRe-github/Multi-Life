ede75f354d65bf44e3754ff6cf5c45da359117dc
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 12:44:23 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -11,7 +11,7 @@ $title[1;$username[$findUser[$message]] Profile]
 $description[1;$addField[1;
 World Details;
 World Name: $replaceText[$getGlobalUserVar[World;$findUser[$message]];0;$username[$findUser[$message]]]
-Total Soldiers: $getGlobalUserVar[Soldiers;$findUser[$message]]
+Total Soldiers: $getGlobalUserVar[Soldier;$findUser[$message]]
 Total Villagers: $getGlobalUserVar[Villager;$findUser[$message]]
 Total People: $getGlobalUserVar[Population;$findUser[$message]]
 $addField[1;Power;
