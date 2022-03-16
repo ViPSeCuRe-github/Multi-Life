7750eca4e662dd3f8c2689601cedf9402b309cc3
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 13:07:30 +0530

    Update help.js

--- help.js
+++ help.js
@@ -3,8 +3,13 @@ module.exports = ((
   code: `$thumbnail[1;$userAvatar[$botID]]
   $title[1;Multi-Life Commands]
   $description[1;
+  $addField[1;Upcoming Updates;
+  \`Battle,Story Command,Clan Chat & Global Chat\`
+  ]
   $addField[1;Clan;
   \`ccreate <clan-name>\`,\`cinfo <ownerID>\`,\`join <ownerID>\`,\`leave\`]
+  $addField[1;Shop;
+  \`shop\`,\`hshop\`,\`buy <item-id>\`,\`create <item-id>\`,\`recruit <hero-name>\`]
   $addField[1;Earn;
   \`work\`,\`daily\`]
   ]`
