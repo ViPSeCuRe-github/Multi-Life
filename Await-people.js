f7129cbe32ad05324471ac16e4ddfb56153f7fbf
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 12:09:02 +0530

    Update Await-people.js

--- Await-people.js
+++ Await-people.js
@@ -1,11 +1,13 @@
 module.exports = ({
-  name: "gems",
+  name: "people",
   type: "awaitedCommand",
   code: `$editMessage[$message[1];
-{title: Gems Shop}
+{title: People Shop}
 {description:  
 100MLC | Villager
 +1 Population • +25MLC In Work Command
+1000MLC | Soldier
++10 Population • +1 Damage
 }
 {footer: You Are In People Page}]`
   })
