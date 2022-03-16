3de9d776ddada45cca844c03cb43eb7f6f74e948
ViPSeCuRe-github <82149278+ViPSeCuRe-github@users.noreply.github.com>
2022-03-16 11:38:10 +0530

    Update buy.js

--- buy.js
+++ buy.js
@@ -5,11 +5,17 @@ module.exports = ({
   $if[$toLowerCase[$message]==aircraft]
   You Have Bought An Aircraft For 10,000 MULTI-LIFE Coins
   $setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];10000]]
+  $setGlobalUserVar[Aircraft;$sum[$getGlobalUserVar[Aircraft];1]]
+  $setGlobalUserVar[Damage;$sum[$getGlobalUserVar[Damage];10]]
+  $onlyIf[$getGlobalUserVar[Aircraft]<=10;You Already Have Bought The Maximum Limit Of Aircrafts]
   $onlyIf[$getGlobalUserVar[Coins]>=10000;You Don't Have 10,000 Multi-Life Coins]
   $endIf
   $if[$toLowerCase[$message]==tank]
   You Have Bought An Aircraft For 15,000 MULTI-LIFE Coins
   $setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];15000]]
+  $setGlobalUserVar[Tank;$sum[$getGlobalUserVar[Tank];1]]
+  $setGlobalUserVar[Damage;$sum[$getGlobalUserVar[Damage];15]]
+  $onlyIf[$getGlobalUserVar[Tank]<=10;You Already Have Bought The Maximum Limit Of Tanks]
   $onlyIf[$getGlobalUserVar[Coins]>=15000;You Don't Have 15,000 Multi-Life Coins]
   $endIf`
   })
